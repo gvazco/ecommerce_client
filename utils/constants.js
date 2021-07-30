d402681596d6e4d4718d6d16727a09f5fc2becc0
Gustavo <gustavovazco@gmail.com>
2021-07-30 18:18:01 -0500

    heroku app

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,5 @@
-export const BASE_PATH = "http://localhost:1337";
+export const BASE_PATH = "https://ecommerce-mevasa.herokuapp.com";
 export const TOKEN = "token";
 export const CART = "cart";
-export const STRIPE_TOKEN = "pk_test_51JGYS8IElWYRXNCG3MzEjVul1xQ5jmkq2B0GLaB8nu1HR60sr0zHmGvCy7Uz2oWZ4dvTqZQb78fudiOK5glYMl0S00hQ773o1g"
+export const STRIPE_TOKEN =
+  "pk_test_51JGYS8IElWYRXNCG3MzEjVul1xQ5jmkq2B0GLaB8nu1HR60sr0zHmGvCy7Uz2oWZ4dvTqZQb78fudiOK5glYMl0S00hQ773o1g";
